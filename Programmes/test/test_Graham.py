6984945c8879438d6d5a8e03556c79552d987ffa
Torkel <edouardgautier@outlook.fr>
2020-05-27 10:19:35 +0200

    Update test

--- Programmes/test/test_Graham.py
+++ Programmes/test/test_Graham.py
@@ -12,6 +12,9 @@ sys.path.append(str(DIRECTORY))
 class test_Graham(unittest.TestCase):
 
     def setUp(self):
+        """
+        On recréer la classe pour eviter toute interaction entre les tests
+        """
         import Class.Graham as Graham
         self.G = Graham.Graham()
         self.G.cloud = [[447, 468], [42, 455], [424, 159], [441, 215],
@@ -20,11 +23,18 @@ class test_Graham(unittest.TestCase):
                         [118, 485], [222, 91], [77, 192], [80, 27], [63, 273],
                         [432, 139], [135, 58], [115, 260], [472, 52],
                         [111, 450], [229, 390], [286, 114], [163, 201]]
+
     def test_origin(self):
+        """
+        Test de la recherche de l origine 
+        """
         self.assertListEqual(self.G.find_origin(), [80, 27],
                              "Mauvaise origine")
 
     def test_sorting_fusion(self):
+        """
+        Test de a methode de tri fusion
+        """
         list_true = [[80, 27], [455, 41], [472, 52], [96, 30], [432, 139],
                      [424, 159], [286, 114], [222, 91], [197, 85], [441, 215],
                      [135, 58], [312, 187], [447, 468], [243, 225], [222, 276],
@@ -36,17 +46,27 @@ class test_Graham(unittest.TestCase):
         self.assertListEqual(list_tets, list_true, "Trie de liste faux")
 
     def test_angle(self):
+        """
+        Test du calcul de l angle
+        """
         self.G.origin = [80, 27]
         self.assertEqual(self.G.angle([42, 455]), 1.6593491796085054,
                          "Erreur de calcul d'angle")
 
     def test_vectorial_product(self):
+        """
+        Test du calcul du produit verctoriel
+        """
         self.assertTrue(self.G.vectorial_product(
             [472, 52], [96, 30], [432, 139]), "Le point C est à gauche de AB")
         self.assertFalse(self.G.vectorial_product(
             [80, 27], [455, 41], [472, 52]), "Le point C est à droite de AB")
 
     def test_fusion(self):
+        """
+        Test de la fusion et tri de deux listes
+        Voir tri fusion
+        """
         self.G.origin = [80, 27]
         list_a_test = [[229, 390], [111, 450]]
         list_b_test = [[286, 114], [163, 201]]
@@ -61,6 +81,9 @@ class test_Graham(unittest.TestCase):
                              "Doit renvoyer une liste vide")
 
     def test_convex_hull(self):
+        """
+        Test global de la recherche de l'enveloppe convexe
+        """
         hull_true = [[80, 27], [455, 41], [472, 52],
                      [447, 468], [118, 485], [42, 455]]
         self.assertListEqual(self.G.convex_hull(self.G.cloud), hull_true,
