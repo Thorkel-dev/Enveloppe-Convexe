5863ff658802736ca3e310e59e655a77a85a0324
Torkel <edouardgautier@outlook.fr>
2020-05-30 17:05:43 +0200

    Test Ugrade

--- Programmes/test/test_Quickhull.py
+++ Programmes/test/test_Quickhull.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest   # Bibiliotheque de test unitaire
 from pathlib import Path    # Appel systeme des paths
 import sys  # Gestion de l interpreteur
 
@@ -9,66 +9,64 @@ DIRECTORY = Path(__file__).parents[1]
 sys.path.append(str(DIRECTORY))
 
 
-class test_Quickhull(unittest.TestCase):
+@pytest.mark.parametrize("cloud", [[[129, 484], [265, 315], [484, 308],
+                                    [308, 361], [425, 308]]])
+@pytest.mark.parametrize("origin_min, origin_max", [([425, 308], [129, 484])])
+@pytest.mark.parametrize("envelope", [[[265, 315], [425, 308], [484, 308],
+                                       [129, 484]]])
+@pytest.mark.parametrize("list_left, list_right", [([[129, 484], [265, 315],
+                                                    [308, 361], [425, 308]],
+                                                    [[484, 308]])])
+class Test_Quickhull():
 
-    def setUp(self):
+    def setup_method(self, method):
         """
         On recréer la classe pour eviter toute interaction entre les tests
         """
         import Class.Quickhull as Quickhull
         self.Q = Quickhull.Quickhull()
-        self.Q.cloud = [[447, 468], [42, 455], [424, 159], [441, 215],
-                        [455, 41], [312, 187], [222, 276], [197, 85],
-                        [64, 249], [243, 225], [111, 228], [96, 30],
-                        [118, 485], [222, 91], [77, 192], [80, 27], [63, 273],
-                        [432, 139], [135, 58], [115, 260], [472, 52],
-                        [111, 450], [229, 390], [286, 114], [163, 201]]
 
-    def test_fin_origin(self):
+    def test_fin_origin(self, cloud, origin_min, origin_max, envelope,
+                        list_right, list_left):
         """
-        Test de la recherche de l origine 
+        Test de la recherche de l origine
         """
-        self.assertTupleEqual(self.Q.find_origin(),
-                              ([80, 27], [118, 485]), "Mauvais origines")
+        self.Q.cloud = cloud
+        assert self.Q.find_origin() == (origin_min, origin_max)
 
-    def test_position_point(self):
+    def test_position_point(self, cloud, origin_min, origin_max, envelope,
+                            list_right, list_left):
         """
         Test de la position du point par rapport a un segment
         """
-        self.assertTrue(self.Q.position_point([80, 27], [118, 485],
-                        [42, 455]), "Le point est à gauche du segment")
-        self.assertFalse(self.Q.position_point([118, 485], [80, 27],
-                         [42, 455]), "Le point est à droite du segment")
+        assert self.Q.position_point([80, 27], [118, 485], [42, 455]) is True
+        assert self.Q.position_point([118, 485], [80, 27], [42, 455]) is False
 
-    def test_distance_point(self):
+    def test_distance_point(self, cloud, origin_min, origin_max, envelope,
+                            list_right, list_left):
         """
         Test du calcul de la distance entre un point et un segment
         """
-        self.assertEqual(self.Q.distance_point([118, 485], [80, 27],
-                         [42, 455]), 15472927.854242455,
-                         "Mauvais calcul de la distance")
+        assert self.Q.distance_point([118, 485], [80, 27], [
+                                     42, 455]) == 15472927.854242455
 
-    def test_point_set(self):
+    def test_point_set(self, cloud, origin_min, origin_max, envelope,
+                       list_right, list_left):
         """
         Test du calcul de la position des points par rapport a un segment
         """
-        list_test = [[222, 91], [77, 192], [80, 27], [63, 273],
-                     [432, 139], [135, 58], [115, 260], [472, 52]]
-        list_right_true = [[222, 91], [432, 139],
-                           [135, 58], [115, 260], [472, 52]]
-        list_left_true = [[77, 192], [80, 27], [63, 273]]
-        self.assertTupleEqual(self.Q.point_set(list_test, [80, 27],
-                              [118, 485]), (list_left_true, list_right_true))
+        assert self.Q.point_set(cloud, origin_min, origin_max) == (
+            list_left, list_right)
 
-    def test_convex_hull(self):
+    def test_convex_hull(self, cloud, origin_min, origin_max, envelope,
+                         list_right, list_left):
         """
         Test global de la recherche de l'enveloppe convexe
         """
-        hull_true = [[42, 455], [80, 27], [455, 41],
-                     [472, 52], [447, 468], [118, 485]]
-        self.assertListEqual(self.Q.convex_hull(self.Q.cloud), hull_true,
-                             "Mauvais calcul de l'enveloppe")
+        self.Q.cloud = cloud
+        assert self.Q.convex_hull(self.Q.cloud) == envelope
 
 
 if __name__ == "__main__":
-    unittest.main()
+    pytest.main(["-v", "Programmes\\test\\Test_Quickhull.py", "--cov",
+                 "--cov-report=html:Programmes\\test\\Quickhull Rapport"])
